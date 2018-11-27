cca281a7f64f4a1c5634389f4baa126f32be00fe
Larrygf02 <raulgf_02@hotmail.com>
2018-11-27 02:51:55 -0500

    integrando con la bd

--- src/containers/RegistroCliente.js
+++ src/containers/RegistroCliente.js
@@ -7,14 +7,35 @@ import { ROUTE } from '../resources/js/Constantes.js'
 
 class RegistroCliente extends Component {
     state = {
-        nombreCompleto  :   '',
+        nombre          :   '',
         correo          :   '',
+        telefono        :   '',
         password        :   '',
     }
 
      handlerLanding = () => {
 		this.props.history.push({pathname : ROUTE.URL_LANDING});
 	}
+
+    handlerSubmit = () => {
+        const { nombre, correo, telefono ,password } = this.state
+        //console.log(this.state)
+        fetch(`http://localhost:4000/usuario/add?nombre=${nombre}&correo=${correo}&telefono=${telefono}&password=${password}`)
+            .then(response => {
+                if (response.status === 200){
+                    console.log('Usuario agregado')
+                    this.props.history.push({pathname : ROUTE.URL_LOGIN_ALUMNO});
+                    //this.setState({nombre: '', correo: '', telefono: '', password: ''})
+                } 
+            })
+            .catch(err => console.err(err))
+        //console.log(correo)
+
+    }
+
+    logChange = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+    }
 
     componentDidMount(){
         scroller.scrollTo('registro', {
@@ -36,28 +57,28 @@ class RegistroCliente extends Component {
                         <div className="row">
                             <div className="input-field col s12">
                                 <i className="material-icons prefix">account_circle</i>
-                                <input id="icon_prefix" type="text" className="validate" />
+                                <input id="icon_prefix" type="text" onChange={this.logChange} className="validate" name="nombre"/>
                                 <label htmlFor="icon_prefix">Nombre Completo</label>
                             </div>
                             <div className="input-field col s12">
                                 <i className="material-icons prefix">phone</i>
-                                <input id="icon_telephone" type="tel" className="validate" />
+                                <input id="icon_telephone" type="tel" onChange={this.logChange} className="validate" name="telefono"/>
                                 <label htmlFor="icon_telephone">Telefono</label>
                             </div>
                             <div className="input-field col s12">
                                 <i className="material-icons prefix">email</i>
-                                <input id="email" type="email" className="validate" />   
+                                <input id="email" type="email" onChange={this.logChange} className="validate" name="correo"/>   
                                 <label htmlFor="email">Correo</label>
                             </div>
                              <div className="input-field col s12">
                                 <i className="material-icons prefix">vpn_key</i>
-                                <input id="password" type="password" className="validate" />
+                                <input id="password" type="password" onChange={this.logChange} className="validate" name="password"/>
                                 <label htmlFor="password">Contraseña</label>
                             </div>
                         </div>
 
                         <div className="btnregistro">
-                            <a className="btn waves-effect waves-light" >Regristrarse</a>
+                            <a className="btn waves-effect waves-light" onClick={this.handlerSubmit}>Regristrarse</a>
                         </div>
                         </form>
                     </div>
